56b03a3fbd4319c18d4f72af6d80b02cffeee75a
tgibbons-css <tgibbons@css.edu>
2017-02-17 11:08:23 -0600

    Initial commit

---
+++ app/src/main/java/css/cis3334/heartratetracker/HeartRateAdapter.java
@@ -0,0 +1,57 @@
+package css.cis3334.heartratetracker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by cssuser on 2/16/2017.
+ */
+
+public class HeartRateAdapter  extends ArrayAdapter<HeartRate> {
+
+    private final Context context;
+    private HeartRateList hrList;
+
+    public HeartRateAdapter(Context context, int rowLayout, int dummyTV, HeartRateList hrList) {
+        super(context, rowLayout, dummyTV, hrList.getList());
+        this.context = context;
+        this.hrList = hrList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //get the heart rate we are displaying
+        HeartRate hr = hrList.getHeartRate(position);
+        View view;
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.heart_rate_row, null);
+
+        TextView tvPulse=(TextView)view.findViewById(R.id.textViewPulse);
+        TextView tvType=(TextView)view.findViewById(R.id.textViewtype);
+        TextView tvDescription=(TextView)view.findViewById(R.id.textViewDescription);
+        tvPulse.setText(hr.getPulse().toString());
+        tvType.setText(hr.getRangeName());
+
+        if (hr.getRange()==1) {
+            tvType.setTextColor(ContextCompat.getColor(context, R.color.colorZone1));
+        } else if (hr.getRange()==3) {
+            tvType.setTextColor(ContextCompat.getColor(context, R.color.colorZone3));
+        } else if (hr.getRange()==5) {
+            tvType.setTextColor(ContextCompat.getColor(context, R.color.colorZone5));
+        }
+
+
+
+        tvDescription.setText(hr.getRangeDescrtiption());
+
+        return(view);
+    }
+
+}
---
+++ app/src/main/java/css/cis3334/heartratetracker/HeartRateList.java
@@ -0,0 +1,44 @@
+package css.cis3334.heartratetracker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by cssuser on 2/16/2017.
+ */
+
+public class HeartRateList {
+    private ArrayList<HeartRate> heartRateList = new ArrayList<HeartRate>();
+
+    public HeartRateList() {
+        heartRateList = new ArrayList<HeartRate>();
+    }
+
+    public void InitRandomYouth() {
+        Random randGenerator = new Random();
+        for (int i=0; i<20; i++) {
+            heartRateList.add(new HeartRate( randGenerator.nextInt(180-110) + 110, 25));
+        }
+    }
+    public void InitRandomElderly() {
+        Random randGenerator = new Random();
+        for (int i=0; i<20; i++) {
+            heartRateList.add(new HeartRate( randGenerator.nextInt(150-80) + 80, 50));
+        }
+    }
+
+    public List getList() {
+        return heartRateList;
+    }
+
+    public HeartRate getHeartRate(Integer index) {
+        return heartRateList.get(index);
+    }
+
+
+    public void remove(Integer index){
+        heartRateList.remove(index);
+    }
+
+}
